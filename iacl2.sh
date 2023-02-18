00cdab9e114b775388f3d07bc7d5d5db04f89dea
jefferson2104 <104745678+jefferson2104@users.noreply.github.com>
2023-02-18 19:17:17 -0300

    Create iacl2.sh

---
+++ iacl2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "atyalizar server"
+
+apt-get update
+apt-get upgrade -y
+
+echo "instalar"
+
+apt-get intall apache2 -y
+apt-get install unzip -y
+
+echo "navigar ate"
+
+cd /tmp
+wgat https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+echo "desconpactar arquivo"
+
+unzip main.zip
+cd /linux-site-dio
+cp -R * /var/www/html/
+
+echo "FIM"
